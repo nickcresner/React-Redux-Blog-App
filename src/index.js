62ddcc6d5c87a377be907f33b508e2516c0dbcbd
nickcresner <nick@wearespork.net>
2017-11-09 10:00:47 +0000

    added react-router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
